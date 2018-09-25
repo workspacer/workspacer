061118e403be07dc9ff8665eb956842a1029ad9a
Rick Button <me@rickbutton.me>
2018-09-24 21:56:14 -0400

    added non-working setup

--- src/Workspacer.Watcher/Properties/AssemblyInfo.cs
+++ src/Workspacer.Watcher/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Workspacer.Watcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("a tiling window manager for Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Workspacer.Watcher")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Rick Button")]
+[assembly: AssemblyProduct("Workspacer")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyVersion("0.2.*")]
