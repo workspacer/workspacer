79bf83334eedf012d3441103137b231d7173edb5
Rick Button <me@rickbutton.me>
2018-09-24 23:47:27 -0400

    switched to wix for installer

---
+++ buildrelease.ps1
@@ -0,0 +1,47 @@
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+
+$setupDir = "src\Workspacer.Setup"
+$config = "Release"
+$platform ="x64"
+
+
+$buildDir = "src\Workspacer\bin\$platform\$config\"
+$barBuildDir = "src\Workspacer.Bar\bin\$platform\$config\"
+
+$infos = Get-ChildItem -Path . -Filter "AssemblyInfo.cs" -Recurse -ErrorAction SilentlyContinue -Force
+$version = Get-Content "VERSION"
+
+$outDir = "out"
+
+if (!(Test-Path $outDir)) {
+    mkdir $outDir
+} else {
+    del out/*
+}
+
+foreach ($file in $infos)
+{
+    "fixing version for $file"
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "AssemblyVersion\("".*""\)", "AssemblyVersion(""$version.*"")" } |
+    Set-Content $file.PSPath
+}
+
+$setupProjs = Get-ChildItem -Path . -Filter "Product.wxs" -Recurse -ErrorAction SilentlyContinue -Force
+foreach ($file in $setupProjs)
+{
+    "fixing version for $file"
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "Version="".*"" Manu", "Version=""$version"" Manu" } |
+    Set-Content $file.PSPath
+}
+
+#& $msbuild Workspacer.sln /t:Clean,Build /p:Configuration=Release /p:Platform=x64
+
+heat dir $buildDir -o $setupDir\Workspacer.wxs -t $setupDir\Workspacer.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer -dr INSTALLDIR -var var.SourceDir
+heat dir $barBuildDir -o $setupDir\Workspacer.Bar.wxs -t $setupDir\Workspacer.Bar.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer.Bar -dr INSTALLDIR -var var.SourceDir
+
+candle $setupDir\Product.wxs -ext WixUIExtension -o $outDir\Product.wixobj 
+candle $setupDir\Workspacer.wxs -ext WixUIExtension -o $outDir\Workspacer.wixobj -dSourceDir="$buildDir"
+candle $setupDir\Workspacer.Bar.wxs -ext WixUIExtension -o $outDir\Workspacer.Bar.wixobj -dSourceDir="$barBuildDir"
+light -out $outDir\Workspacer.$version.msi $outDir\Product.wixobj $outDir\Workspacer.wixobj $outDir\Workspacer.Bar.wixobj -ext WixUIExtension
---
+++ src/Workspacer.Setup/Workspacer.xslt
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<xsl:stylesheet version="1.0" 
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:wix="http://schemas.microsoft.com/wix/2006/wi"
+                xmlns="http://schemas.microsoft.com/wix/2006/wi"
+                exclude-result-prefixes="xsl wix">
+
+  <xsl:output method="xml" indent="yes" omit-xml-declaration="yes" />
+
+  <xsl:strip-space elements="*"/>
+
+  <xsl:key name="ToRemove"
+           match="wix:Component[contains(wix:File/@Source, '.pdb') or contains(wix:File/@Source, '.xml')]"
+           use="@Id" />
+
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()"/>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="*[self::wix:Component or self::wix:ComponentRef]
+                        [key('ToRemove', @Id)]" />
+</xsl:stylesheet>
