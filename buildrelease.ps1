ed3975296a9426a8ad2df69d6de9b5aef1e3314f
Rick Button <me@rickbutton.me>
2018-11-18 21:02:49 -0500

    Added focusindicator to wix build

--- buildrelease.ps1
+++ buildrelease.ps1
@@ -8,6 +8,7 @@ $platform ="x64"
 $buildDir = "src\Workspacer\bin\$platform\$config\"
 $barBuildDir = "src\Workspacer.Bar\bin\$platform\$config\"
 $menuBuildDir = "src\Workspacer.ActionMenu\bin\$platform\$config\"
+$focusBuildDir = "src\Workspacer.FocusIndicator\bin\$platform\$config\"
 
 $infos = Get-ChildItem -Path . -Filter "AssemblyInfo.cs" -Recurse -ErrorAction SilentlyContinue -Force
 $version = Get-Content "VERSION"
@@ -42,9 +43,11 @@ foreach ($file in $setupProjs)
 heat dir $buildDir -o $setupDir\Workspacer.wxs -t $setupDir\Workspacer.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer -dr INSTALLDIR -var var.SourceDir
 heat dir $barBuildDir -o $setupDir\Workspacer.Bar.wxs -t $setupDir\Workspacer.Bar.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer.Bar -dr INSTALLDIR -var var.SourceDir
 heat dir $menuBuildDir -o $setupDir\Workspacer.ActionMenu.wxs -t $setupDir\Workspacer.ActionMenu.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer.ActionMenu -dr INSTALLDIR -var var.SourceDir
+heat dir $focusBuildDir -o $setupDir\Workspacer.FocusIndicator.wxs -t $setupDir\Workspacer.FocusIndicator.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer.FocusIndicator -dr INSTALLDIR -var var.SourceDir
 
 candle $setupDir\Product.wxs -ext WixUIExtension -o $outDir\Product.wixobj 
 candle $setupDir\Workspacer.wxs -ext WixUIExtension -o $outDir\Workspacer.wixobj -dSourceDir="$buildDir"
 candle $setupDir\Workspacer.Bar.wxs -ext WixUIExtension -o $outDir\Workspacer.Bar.wixobj -dSourceDir="$barBuildDir"
 candle $setupDir\Workspacer.ActionMenu.wxs -ext WixUIExtension -o $outDir\Workspacer.ActionMenu.wixobj -dSourceDir="$menuBuildDir"
-light -out $outDir\Workspacer-$version.msi $outDir\Product.wixobj $outDir\Workspacer.wixobj $outDir\Workspacer.Bar.wixobj $outdir\Workspacer.ActionMenu.wixobj -ext WixUIExtension
+candle $setupDir\Workspacer.FocusIndicator.wxs -ext WixUIExtension -o $outDir\Workspacer.FocusIndicator.wixobj -dSourceDir="$focusBuildDir"
+light -out $outDir\Workspacer-$version.msi $outDir\Product.wixobj $outDir\Workspacer.wixobj $outDir\Workspacer.Bar.wixobj $outdir\Workspacer.ActionMenu.wixobj $outdir\Workspacer.FocusIndicator.wixobj -ext WixUIExtension
