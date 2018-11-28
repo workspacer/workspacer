43945c434b4ca152a004c29be49d350945ade300
Rick Button <me@rickbutton.me>
2018-11-28 00:09:47 -0500

    updated buildsite

--- scripts/buildsite.ps1
+++ scripts/buildsite.ps1
@@ -1,1 +1,10 @@
-hugo --config src/docs/config.toml -s src/docs
+git checkout gh-pages
+git checkout master -- docs
+git reset HEAD .
+
+hugo --config docs/config.toml -s docs
+
+remove-item -force -recurse docs
+
+git add .
+git commit -m ".\scripts\buildsite.ps1 - generated site at $(Get-Date)"
