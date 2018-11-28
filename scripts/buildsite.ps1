7505e47de88c970c7e9f1da045854f22dca1479c
Rick Button <me@rickbutton.me>
2018-11-28 00:48:33 -0500

    path

--- scripts/buildsite.ps1
+++ scripts/buildsite.ps1
@@ -6,16 +6,9 @@ git checkout gh-pages
 git checkout $sourceCommit -- docs
 git reset HEAD .
 
-$toolsPath = "$PSScriptRoot\tools\"
-$oldPath = $env:PATH
-
-$env:PATH="$toolsPath;$oldPath"
-
-hugo --config docs/config.toml -s docs
+& ".\tools\hugo.exe" --config docs/config.toml -s docs
 
 remove-item -force -recurse docs
 
 git add .
 git commit -m ".\scripts\buildsite.ps1 - generated site at $(Get-Date)"
-
-$env:PATH = $oldPath
