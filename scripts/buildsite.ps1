f7a8765b017b6d588324b7059e1289639702790f
Rick Button <me@rickbutton.me>
2018-11-28 00:46:58 -0500

    path

--- scripts/buildsite.ps1
+++ scripts/buildsite.ps1
@@ -6,7 +6,7 @@ git checkout gh-pages
 git checkout $sourceCommit -- docs
 git reset HEAD .
 
-$toolsPath = "$PSScriptRoot\tools\hugo\"
+$toolsPath = "$PSScriptRoot\tools\"
 $oldPath = $env:PATH
 
 $env:PATH="$toolsPath;$oldPath"
