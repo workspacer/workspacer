a68f9e7c5c1bcb42d9efb4f26bf6ddb78e8a5a2e
Rick Button <me@rickbutton.me>
2018-11-27 23:37:00 -0500

    updated install script

--- scripts/buildinstaller.ps1
+++ scripts/buildinstaller.ps1
@@ -1,12 +1,10 @@
  param (
-    [string]$config,
-    [string]$platform,
+    [string]$buildDir,
     [string]$version = ""
  )
 
 $setupDir = "src\Workspacer.Setup"
 
-$buildDir = "src\Workspacer\bin\$platform\$config\"
 $outDir = "out"
 $wixDir = "wix"
 
@@ -20,8 +18,8 @@ if (![string]::IsNullOrEmpty($version)) {
     $version = "-$version"
 }
 
-& ".\$wixDir\heat.exe" dir $buildDir -o $setupDir\Workspacer.wxs -t $setupDir\Workspacer.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer -dr INSTALLDIR -var var.SourceDir
+& ".\$wixDir\heat.exe" dir $buildDir -o $outDir\Workspacer.wxs -t $setupDir\Workspacer.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer -dr INSTALLDIR -var var.SourceDir
 
 & ".\$wixDir\candle.exe" $setupDir\Product.wxs -ext WixUIExtension -o $outDir\Product.wixobj 
-& ".\$wixDir\candle.exe" $setupDir\Workspacer.wxs -ext WixUIExtension -o $outDir\Workspacer.wixobj -dSourceDir="$buildDir"
+& ".\$wixDir\candle.exe" $outDir\Workspacer.wxs -ext WixUIExtension -o $outDir\Workspacer.wixobj -dSourceDir="$buildDir"
 & ".\$wixDir\light.exe" -out $outDir\Workspacer$version.msi $outDir\Product.wixobj $outDir\Workspacer.wixobj -ext WixUIExtension
