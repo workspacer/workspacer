9ada27056d867c0621b0db766937a30ef75fb42e
Rick Button <me@rickbutton.me>
2018-11-28 21:37:45 -0500

    updated build site script

--- scripts/buildsite.ps1
+++ scripts/buildsite.ps1
@@ -1,14 +1,2 @@
-$sourceCommit = (git rev-parse HEAD) | Out-String
-$sourceCommit = $sourceCommit -replace "`n","" -replace "`r",""
-$sourceCommit = $sourceCommit.Trim()
-
-git checkout gh-pages
-git checkout $sourceCommit -- docs
-git reset HEAD .
 
 & ".\tools\hugo.exe" --config docs/config.toml -s docs
-
-remove-item -force -recurse docs
-
-git add .
-git commit -m ".\scripts\buildsite.ps1 - generated site at $(Get-Date)"
