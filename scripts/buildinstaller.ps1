c9f77c073773414a8d97d9c553d68374bd02aebd
Rick Button <me@rickbutton.me>
2018-11-28 21:44:16 -0500

    updated installer script

--- scripts/buildinstaller.ps1
+++ scripts/buildinstaller.ps1
@@ -6,7 +6,6 @@
 $setupDir = "src\Workspacer.Setup"
 
 $outDir = "out"
-$wixDir = "wix"
 
 if (!(Test-Path $outDir)) {
     mkdir $outDir
@@ -18,8 +17,8 @@ if (![string]::IsNullOrEmpty($version)) {
     $version = "-$version"
 }
 
-& ".\$wixDir\heat.exe" dir $buildDir -o $outDir\Workspacer.wxs -t $setupDir\Workspacer.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer -dr INSTALLDIR -var var.SourceDir
+& ".\tools\wix\heat.exe" dir $buildDir -o $outDir\Workspacer.wxs -t $setupDir\Workspacer.xslt -scon -sfrag -srd -sreg -gg -cg Workspacer -dr INSTALLDIR -var var.SourceDir
 
-& ".\$wixDir\candle.exe" $setupDir\Product.wxs -ext WixUIExtension -o $outDir\Product.wixobj 
-& ".\$wixDir\candle.exe" $outDir\Workspacer.wxs -ext WixUIExtension -o $outDir\Workspacer.wixobj -dSourceDir="$buildDir"
-& ".\$wixDir\light.exe" -out $outDir\Workspacer$version.msi $outDir\Product.wixobj $outDir\Workspacer.wixobj -ext WixUIExtension
+& ".\tools\wix\candle.exe" $setupDir\Product.wxs -ext WixUIExtension -o $outDir\Product.wixobj 
+& ".\tools\wix\candle.exe" $outDir\Workspacer.wxs -ext WixUIExtension -o $outDir\Workspacer.wixobj -dSourceDir="$buildDir"
+& ".\tools\wix\light.exe" -out $outDir\Workspacer$version.msi $outDir\Product.wixobj $outDir\Workspacer.wixobj -ext WixUIExtension
