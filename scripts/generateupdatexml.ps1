7bf48a840dfbf4521797ba15c51a13d1661e244e
Rick Button <me@rickbutton.me>
2018-11-29 00:25:25 -0500

    updated generateupdatexml script

--- scripts/generateupdatexml.ps1
+++ scripts/generateupdatexml.ps1
@@ -1,6 +1,5 @@
  param (
     [string]$version,
-    [string]$build,
     [string]$url,
     [string]$out
  )
@@ -8,7 +7,6 @@
 $xml = Get-Content "scripts/update-template.xml"
 
 $xml = $xml -replace "REPLACE_VERSION", $version
-$xml = $xml -replace "REPLACE_BUILD", $build
 $xml = $xml -replace "REPLACE_URL", $url
 
 $xml | Set-Content $out
