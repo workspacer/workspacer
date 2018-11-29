f4c06d23709de39fa1ec7decb3d75759cadb53b5
Rick Button <me@rickbutton.me>
2018-11-28 23:53:28 -0500

    bumped version to v0.9.2

--- src/Workspacer.ConfigLoader/Properties/AssemblyInfo.cs
+++ src/Workspacer.ConfigLoader/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.9.1")]
-[assembly: AssemblyVersion("0.9.1")]
+// [assembly: AssemblyVersion("0.9.2")]
+[assembly: AssemblyVersion("0.9.2")]
