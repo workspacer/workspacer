c678553bb86cafa963c70d99da6019f7257a0f26
Rick Button <me@rickbutton.me>
2019-01-03 23:40:28 -0500

    added WindowRouter helpers!

--- src/workspacer.Shared/Workspace/IWindowRouter.cs
+++ src/workspacer.Shared/Workspace/IWindowRouter.cs
@@ -13,5 +13,14 @@ namespace workspacer
         void ClearRoutes();
         void AddFilter(Func<IWindow, bool> filter);
         void AddRoute(Func<IWindow, IWorkspace> route);
+
+        void IgnoreWindowClass(string windowClass);
+        void IgnoreProcessName(string processName);
+
+        void RouteWindowClass(string windowClass, string workspaceName);
+        void RouteWindowClass(string windowClass, IWorkspace workspace);
+
+        void RouteProcessName(string processName, string workspaceName);
+        void RouteProcessName(string processName, IWorkspace workspace);
     }
 }
