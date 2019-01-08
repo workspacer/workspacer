daaf11030887c348b3c1d5ddc12e304abf966efa
Rick Button <me@rickbutton.me>
2019-01-07 22:10:10 -0500

    added WindowRouter helpers for window titles

--- src/workspacer.Shared/Workspace/IWindowRouter.cs
+++ src/workspacer.Shared/Workspace/IWindowRouter.cs
@@ -16,11 +16,18 @@ namespace workspacer
 
         void IgnoreWindowClass(string windowClass);
         void IgnoreProcessName(string processName);
+        void IgnoreTitle(string title);
+        void IgnoreTitleMatch(string match);
 
         void RouteWindowClass(string windowClass, string workspaceName);
         void RouteWindowClass(string windowClass, IWorkspace workspace);
 
         void RouteProcessName(string processName, string workspaceName);
         void RouteProcessName(string processName, IWorkspace workspace);
+
+        void RouteTitle(string title, string workspaceName);
+        void RouteTitle(string title, IWorkspace workspace);
+        void RouteTitleMatch(string match, string workspaceName);
+        void RouteTitleMatch(string match, IWorkspace workspace);
     }
 }
