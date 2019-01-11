3cb5482dcc03fb4de555dcf85f1ba1e0cb91954f
Rick Button <me@rickbutton.me>
2019-01-10 19:11:23 -0500

    fixed crash after restart, after monitors added to system

--- src/workspacer.Shared/Workspace/WorkspaceState.cs
+++ src/workspacer.Shared/Workspace/WorkspaceState.cs
@@ -9,7 +9,7 @@ namespace workspacer
     public class WorkspaceState
     {
         public List<List<int>> WorkspacesToWindows { get; set; }
-        public Dictionary<int, int> MonitorsToWorkspaces { get; set; }
+        public List<int> MonitorsToWorkspaces { get; set; }
         public int FocusedMonitor { get; set; }
         public int FocusedWindow { get; set; }
     }
