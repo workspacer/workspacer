59972172b424c039cdfce08afc35fc1b46a1936d
Rick Button <me@rickbutton.me>
2019-02-26 01:07:12 -0500

    switched to new keybd_event hack for FocusStealer

--- src/workspacer.Native/Native/FocusStealer.cs
+++ src/workspacer.Native/Native/FocusStealer.cs
@@ -16,10 +16,8 @@ namespace workspacer
 
         public static void Steal(IntPtr windowToFocus)
         {
-            Win32.AllocConsole();
-            var hWndConsole = Win32.GetConsoleWindow();
-            Win32.SetWindowPos(hWndConsole, IntPtr.Zero, 0, 0, 0, 0, Win32.SetWindowPosFlags.IgnoreZOrder);
-            Win32.FreeConsole();
+            Win32.keybd_event(0, 0, 0, UIntPtr.Zero);
+
             Win32.SetForegroundWindow(windowToFocus);
         }
     }
