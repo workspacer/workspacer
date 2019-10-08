d709255bfdb280c90ca42a17425e64cd38c25158
Jostein Kjønigsen <jostein@kjonigsen.net>
2019-10-08 12:37:02 +0200

    Add XUnit test-project to solution.

---
+++ src/workspacer.Shared.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("workspacer.Shared.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("workspacer.Shared.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c69c243c-e40c-46cc-8599-dc7cc39f4c6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/workspacer.Shared.Tests/XunitRunnerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace workspacer.Shared.Tests
+{
+    public class XunitRunnerTests
+    {
+        [Fact]
+        public void Can_Run_Test()
+        {
+        }
+    }
+}
