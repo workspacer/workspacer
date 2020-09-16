2f45b5c2db94279f7e91aeaebd9de26f7c8b0df0
haoxiangliew <62730710+haoxiangliew@users.noreply.github.com>
2020-09-15 23:41:27 -0400

    Add files via upload

--- src/workspacer.Bar/Widgets/BatteryWidget.cs
+++ src/workspacer.Bar/Widgets/BatteryWidget.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -11,8 +11,12 @@ namespace workspacer.Bar.Widgets
     public class BatteryWidget : BarWidgetBase
     {
         public Color LowChargeColor { get; set; } = Color.Red;
+        public Color MedChargeColor { get; set; } = Color.Yellow;
+        public Color HighChargeColor { get; set; } = Color.Green;
         public bool HasBatteryWarning { get; set; } = true;
         public double LowChargeThreshold { get; set; } = 0.10;
+        public double MedChargeThreshold { get; set; } = 0.50;
+        public double HighChargeThreshold { get; set; } = 1.00;
         public int Interval { get; set; } = 5000;
 
         private System.Timers.Timer _timer;
@@ -28,23 +32,26 @@ namespace workspacer.Bar.Widgets
                 {
                     return Parts(Part(currentBatteryCharge.ToString("#0%"), LowChargeColor));
                 }
+                else if (currentBatteryCharge <= MedChargeThreshold)
+                {
+                    return Parts(Part(currentBatteryCharge.ToString("#0%"), MedChargeColor));
+                }
+                else if (currentBatteryCharge <= HighChargeThreshold)
+                {
+                    return Parts(Part(currentBatteryCharge.ToString("#0%"), HighChargeColor));
+                }
                 else
                 {
                     return Parts(Part(currentBatteryCharge.ToString("#0%")));
                 }
             }
-            else
-            {
-                return Parts(currentBatteryCharge.ToString("#0%"));
-            }
-        }
 
-        public override void Initialize()
-        {
-            _timer = new System.Timers.Timer(Interval);
-            _timer.Elapsed += (s, e) => Context.MarkDirty();
-            _timer.Enabled = true;
+            public override void Initialize()
+            {
+                _timer = new System.Timers.Timer(Interval);
+                _timer.Elapsed += (s, e) => Context.MarkDirty();
+                _timer.Enabled = true;
+            }
         }
     }
 }
-
