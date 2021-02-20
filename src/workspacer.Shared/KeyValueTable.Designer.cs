e22786db365871604a506a9dcf1dc8acba0e037a
dalyIsaac <isaac.daly@outlook.com>
2021-02-20 23:41:04 +1300

    `ShowKeybindDialog` now renders a table. - Added `KeyValueTable` - `GetKeybindString` capitalizes keys - `GetKeybindString` adds support for `KeyModifiers.Win`

---
+++ src/workspacer.Shared/KeyValueTable.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace workspacer
+{
+    partial class KeyValueTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.SubtitleText = new System.Windows.Forms.Label();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToAddRows = false;
+            this.DataGridView.AllowUserToDeleteRows = false;
+            this.DataGridView.AllowUserToOrderColumns = true;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(18, 83);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.ReadOnly = true;
+            this.DataGridView.RowTemplate.Height = 25;
+            this.DataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DataGridView.Size = new System.Drawing.Size(882, 442);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // SubtitleText
+            // 
+            this.SubtitleText.AutoSize = true;
+            this.SubtitleText.Location = new System.Drawing.Point(14, 10);
+            this.SubtitleText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SubtitleText.Name = "SubtitleText";
+            this.SubtitleText.Size = new System.Drawing.Size(0, 15);
+            this.SubtitleText.TabIndex = 2;
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(18, 54);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.PlaceholderText = "Search";
+            this.SearchBox.Size = new System.Drawing.Size(882, 23);
+            this.SearchBox.TabIndex = 3;
+            this.SearchBox.TextChanged += new System.EventHandler(this.SearchBox_TextChanged);
+            // 
+            // KeyValueTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 532);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.SubtitleText);
+            this.Controls.Add(this.DataGridView);
+            this.Name = "KeyValueTable";
+            this.Text = "KeyValueTable";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Label SubtitleText;
+        private System.Windows.Forms.TextBox SearchBox;
+    }
+}
