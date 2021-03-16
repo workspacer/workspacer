7953c5bec8be9bf6a61419e72c9d887f4c6e1589
dalyIsaac <isaac.daly@outlook.com>
2021-03-17 11:10:49 +1300

    Added primary area to PaneLayoutEngines

--- src/workspacer.Shared/Layout/PaneLayoutEngine.cs
+++ src/workspacer.Shared/Layout/PaneLayoutEngine.cs
@@ -22,38 +22,61 @@ namespace workspacer
     {
         private readonly bool _vertical;
 
+        private int _primaryIdx;
+
+        private readonly double _primaryPercentIncrement;
+
+        private double _primaryPercentOffset = 0;
+
         public abstract string Name { get; }
 
-        public PaneLayoutEngine(bool vertical)
+        public PaneLayoutEngine(bool vertical) : this(vertical, 0, 0.03) { }
+
+        public PaneLayoutEngine(bool vertical, int primaryIdx, double primaryPercentIncrement)
         {
             _vertical = vertical;
+            _primaryIdx = primaryIdx;
+            _primaryPercentIncrement = primaryPercentIncrement;
         }
 
         public IEnumerable<IWindowLocation> CalcLayout(IEnumerable<IWindow> windows, int spaceWidth, int spaceHeight)
         {
             var list = new List<IWindowLocation>();
             var numWindows = windows.Count();
+            _primaryIdx %= numWindows;
 
             if (numWindows == 0)
                 return list;
 
+            double currentPercent = (1.0D / numWindows) + _primaryPercentOffset;
             if (_vertical)
             {
-                int width = (spaceWidth / numWindows);
-                int height = spaceHeight;
-
-                for (var i = 0; i < numWindows; i++)
-                {
-                    list.Add(new WindowLocation(i * width, 0, width, height, WindowState.Normal));
-                }
-            } else
-            {
-                int height = (spaceHeight / numWindows);
+                int primaryHeight = (int)(spaceHeight * currentPercent);
                 int width = spaceWidth;
+                int height = numWindows <= 1 ? spaceHeight : ((spaceHeight - primaryHeight) / (numWindows - 1));
+                int startY = 0;
 
                 for (var i = 0; i < numWindows; i++)
                 {
-                    list.Add(new WindowLocation(0, i * height, width, height, WindowState.Normal));
+                    int currentHeight = i == _primaryIdx ? primaryHeight : height;
+
+                    list.Add(new WindowLocation(0, startY, width, currentHeight, WindowState.Normal));
+                    startY += currentHeight;
+                }
+            }
+            else
+            {
+                int primaryWidth = (int)(spaceWidth * currentPercent);
+                int width = numWindows <= 1 ? spaceWidth : ((spaceWidth - primaryWidth) / (numWindows - 1));
+                int height = spaceHeight;
+                int startX = 0;
+
+                for (var i = 0; i < numWindows; i++)
+                {
+                    int currentWidth = i == _primaryIdx ? primaryWidth : width;
+
+                    list.Add(new WindowLocation(startX, 0, currentWidth, height, WindowState.Normal));
+                    startX += currentWidth;
                 }
             }
 
@@ -61,9 +84,19 @@ namespace workspacer
         }
 
         public void DecrementNumInPrimary() { }
-        public void ExpandPrimaryArea() { }
         public void IncrementNumInPrimary() { }
-        public void ResetPrimaryArea() { }
-        public void ShrinkPrimaryArea() { } 
+        public void ExpandPrimaryArea()
+        {
+            _primaryPercentOffset += _primaryPercentIncrement;
+        }
+
+        public void ResetPrimaryArea()
+        {
+            _primaryPercentOffset = 0;
+        }
+        public void ShrinkPrimaryArea()
+        {
+            _primaryPercentOffset -= _primaryPercentIncrement;
+        }
     }
 }
