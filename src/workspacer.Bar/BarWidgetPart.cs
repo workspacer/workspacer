4d22bf0d55964bdf62a8f58f814da7c781765b1f
John <johnschiltz2000@gmail.com>
2021-06-27 20:56:52 -0500

    Added Left and Right Padding to comply with IBarWidget Interface

--- src/workspacer.Bar/BarWidgetPart.cs
+++ src/workspacer.Bar/BarWidgetPart.cs
@@ -9,6 +9,8 @@ namespace workspacer.Bar
     public class BarWidgetPart : IBarWidgetPart
     {
         public string Text { get; set; }
+        public string LeftPadding { get; set; }
+        public string RightPadding { get; set; }
         public Color ForegroundColor { get; set; }
         public Color BackgroundColor { get; set; }
         public Action PartClicked { get; set; }
