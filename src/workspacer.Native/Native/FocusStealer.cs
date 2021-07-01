4c1c0ba32ab5c1bd146e81202a3162b884b3c7f1
Mateo Carriquí <mateocarriqui7@gmail.com>
2021-07-01 20:11:10 -0300

    Reverted to AllocConsole method
    See #110, I'm having this exact problem using the Win key, the start menu appears when I switch workspaces while focusing a window. Not sure why @rickbutton did this change when this was working.

--- src/workspacer.Native/Native/FocusStealer.cs
+++ src/workspacer.Native/Native/FocusStealer.cs
@@ -16,8 +16,11 @@ namespace workspacer
 
         public static void Steal(IntPtr windowToFocus)
         {
-            Win32.keybd_event(0, 0, 0, UIntPtr.Zero);
-
+            Win32.AllocConsole();
+            var hWndConsole = Win32.GetConsoleWindow();
+            Win32.SetWindowPos(hWndConsole, IntPtr.Zero, 0, 0, 0, 0, Win32.SetWindowPosFlags.IgnoreZOrder);
+            Win32.FreeConsole();
+            
             Win32.SetForegroundWindow(windowToFocus);
         }
     }
