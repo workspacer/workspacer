1afac4697c61d2b85081f820c2f1272a0c808c3e
dalyIsaac <isaac.daly@outlook.com>
2021-07-24 17:43:01 +1200

    Updated default style in `TitleBarPluginConfig`

--- src/workspacer.TitleBar/TitleBarPluginConfig.cs
+++ src/workspacer.TitleBar/TitleBarPluginConfig.cs
@@ -1,44 +1,39 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using workspacer.TitleBar;
 
 namespace workspacer.TitleBar
 {
     public class TitleBarPluginConfig
     {
-        public bool ShowTitleBars { get; set; }
+        public TitleBarStyle DefaultStyle { get; set; }
         public List<TitleBarRule> Rules { get; set; }
 
-        public TitleBarPluginConfig() : this(false)
+        public TitleBarPluginConfig() : this(new TitleBarStyle(true, true))
         {
         }
 
-        public TitleBarPluginConfig(bool hideTitleBars)
+        public TitleBarPluginConfig(TitleBarStyle defaultStyle)
         {
-            ShowTitleBars = hideTitleBars;
+            DefaultStyle = defaultStyle;
             Rules = new List<TitleBarRule>();
         }
 
-        public void SetWindowClass(string windowClass, TitleBarStyle style = null)
+        public void SetWindowClass(string windowClass, TitleBarStyle style)
         {
             Rules.Add(new TitleBarRule(window => window.Class == windowClass, style));
         }
 
-        public void SetWindowProcessName(string processName, TitleBarStyle style = null)
+        public void SetWindowProcessName(string processName, TitleBarStyle style)
         {
             Rules.Add(new TitleBarRule(window => window.ProcessName == processName, style));
         }
 
-        public void SetWindowTitle(string title, TitleBarStyle style = null)
+        public void SetWindowTitle(string title, TitleBarStyle style)
         {
             Rules.Add(new TitleBarRule(window => window.Title == title, style));
         }
 
-        public void SetWindowTitleMAtch(string match, TitleBarStyle style = null)
+        public void SetWindowTitleMAtch(string match, TitleBarStyle style)
         {
             var regex = new Regex(match);
             Rules.Add(new TitleBarRule(window => regex.IsMatch(window.Title), style));
