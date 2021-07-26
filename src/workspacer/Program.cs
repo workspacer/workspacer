c9363b78c5173b845ef78b3875a42b305e4e7bcf
Ryan Caezar Itang <sitiom@disroot.org>
2021-07-26 23:39:46 +0800

    Update Program.cs

--- src/workspacer/Program.cs
+++ src/workspacer/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 using Application = System.Windows.Forms.Application;
 using Timer = System.Timers.Timer;
