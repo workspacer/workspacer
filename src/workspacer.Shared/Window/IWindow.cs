1bb5e670ca9814477256231bb44ad648035b16de
tlsomers <56299065+tlsomers@users.noreply.github.com>
2022-08-29 01:20:35 -0700

    Remove a lot of unused imports (#341)

--- src/workspacer.Shared/Window/IWindow.cs
+++ src/workspacer.Shared/Window/IWindow.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace workspacer
 {
--- src/workspacer/Workspace/WorkspaceManager.cs
+++ src/workspacer/Workspace/WorkspaceManager.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
-using System.Dynamic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace workspacer
