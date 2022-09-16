dfdafd68933c5c9c57e1568f740b8742e0f2f78a
tlsomers <56299065+tlsomers@users.noreply.github.com>
2022-09-16 09:31:56 -0700

    Permanent border on focus window plugin (#351)
    * Create focus border plugin for permanent focussed window border.
    
    * Resolve tabs in workspacer.csproj
    
    Co-authored-by: tomtrein <tomtrein@famsomers.com>

---
+++ src/workspacer.FocusBorder/FocusBorderPluginConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace workspacer.FocusBorder
+{
+    public class FocusBorderPluginConfig
+    {
+        /// <summary>
+        /// The color of the focus border.
+        /// </summary>
+        public Color BorderColor = Color.Red;
+        
+        /// <summary>
+        /// The width of the focus border in px.
+        /// </summary>
+        public int BorderSize = 5;
+
+        /// <summary>
+        /// The opacity of the focus border between 0 (completely transparent) and 1 (completely opaque).
+        /// </summary>
+        public double Opacity = 1.0;
+    }
+}
