19f4e7b4cfab79447359459f69b94873c8723632
tomtrein <tomtrein@famsomers.com>
2022-09-22 02:23:37 -0700

    Resolve consistency in extension methods

--- src/workspacer.Bar/ConfigContextExtensions.cs
+++ src/workspacer.Bar/ConfigContextExtensions.cs
@@ -4,22 +4,22 @@ namespace workspacer.Bar
 {
     public static class ConfigContextExtensions
     {
-        public static void AddBar(this IConfigContext context, BarPluginConfig config = null)
+        public static BarPlugin AddBar(this IConfigContext context, BarPluginConfig config = null)
         {
             config = config ?? new BarPluginConfig();
 
             context.AddLayoutProxy((layout) => new MenuBarLayoutEngine(layout, config.BarTitle, config.BarHeight));
 
-            context.Plugins.RegisterPlugin(new BarPlugin(config));
+            return context.Plugins.RegisterPlugin(new BarPlugin(config));
         }
         
-        public static void AddActionMenu(
+        public static BarPlugin AddBar(
             this IConfigContext context,
             Action<BarPluginConfig> configAction)
         {
             var config = new BarPluginConfig();
             configAction.Invoke(config);
-            AddBar(context, config);
+            return AddBar(context, config);
         }
     }
 }
--- src/workspacer.FocusIndicator/ConfigContextExtensions.cs
+++ src/workspacer.FocusIndicator/ConfigContextExtensions.cs
@@ -4,16 +4,16 @@ namespace workspacer.FocusIndicator
 {
     public static class ConfigContextExtensions
     {
-        public static void AddFocusIndicator(this IConfigContext context, FocusIndicatorPluginConfig config = null)
+        public static FocusIndicatorPlugin AddFocusIndicator(this IConfigContext context, FocusIndicatorPluginConfig config = null)
         {
-            context.Plugins.RegisterPlugin(new FocusIndicatorPlugin(config ?? new FocusIndicatorPluginConfig()));
+            return context.Plugins.RegisterPlugin(new FocusIndicatorPlugin(config ?? new FocusIndicatorPluginConfig()));
         }
         
-        public static void AddFocusIndicator(this IConfigContext context, Action<FocusIndicatorPluginConfig> onConfig)
+        public static FocusIndicatorPlugin AddFocusIndicator(this IConfigContext context, Action<FocusIndicatorPluginConfig> onConfig)
         {
             var config = new FocusIndicatorPluginConfig();
             onConfig(config);
-            context.AddFocusIndicator(config);
+            return context.AddFocusIndicator(config);
         }
     }
 }
