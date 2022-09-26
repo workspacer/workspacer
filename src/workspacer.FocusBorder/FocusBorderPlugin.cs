9ab1bbd7500d18eb4b6167747036bed47bd06c4f
tomtrein <tomtrein@famsomers.com>
2022-09-26 12:28:43 +0200

    Improve movement tracking with border and fix minimization and fullscreen

--- src/workspacer.FocusBorder/FocusBorderPlugin.cs
+++ src/workspacer.FocusBorder/FocusBorderPlugin.cs
@@ -6,7 +6,7 @@ namespace workspacer.FocusBorder
     {
         private IConfigContext _context;
         private FocusBorderPluginConfig _config;
-        private FocusBorderForm _form;
+        private IFormProxy<FocusBorderForm> _form;
 
         public FocusBorderPlugin(FocusBorderPluginConfig config)
         {
@@ -16,14 +16,19 @@ namespace workspacer.FocusBorder
         public void AfterConfig(IConfigContext context)
         {
             this._context = context;
-            this._form = new FocusBorderForm(this._config);
-            this._form.Show();
+            
+            var form = new FocusBorderForm(this._config);
+            this._form = form;
+            
+            form.Show();
             
             // Update the focus border when one of the workspaces updated its layout.
             foreach (var workspace in context.WorkspaceContainer.GetAllWorkspaces())
             {
                 workspace.OnLayoutCompleted += OnLayoutCompleted;
             }
+
+            ((WindowsManager) context.Windows).WindowUpdated += WindowUpdated;
             
             _context.Workspaces.FocusedMonitorUpdated += RefreshFocusedMonitor;
 
@@ -32,7 +37,7 @@ namespace workspacer.FocusBorder
                 context.Workspaces.FocusedWorkspace.ManagedWindows.FirstOrDefault(x => x.IsFocused);
             initial ??= context.Workspaces.FocusedWorkspace.ManagedWindows.FirstOrDefault();
             if (initial is not null)
-                this._form.SetWindow(initial);
+                form.SetWindow(initial);
         }
 
         private void OnLayoutCompleted(IWorkspace workspace)
@@ -44,14 +49,22 @@ namespace workspacer.FocusBorder
             // If there is no focused window, hide the border,
             if (workspace.FocusedWindow is not null)
             {
-                if (!this._form.Visible)
-                    this._form.Show();
+                if (!this._form.Read.Visible)
+                    this._form.Execute(x => x.Show());
                 if (workspace.FocusedWindow.CanLayout)
-                    this._form.SetWindow(workspace.FocusedWindow);
+                    this._form.Execute(x => x.SetWindow(workspace.FocusedWindow));
             }
             else
             {
-                this._form.Hide();
+                this._form.Execute(x => x.Hide());
+            }
+        }
+
+        private void WindowUpdated(IWindow window, WindowUpdateType type)
+        {
+            if (type == WindowUpdateType.MinimizeStart && this._form.Read.Current == window)
+            {
+                this._form.Execute(x => x.Hide());
             }
         }
 
---
+++ src/workspacer.FocusBorder/IFormProxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace workspacer.FocusBorder
+{
+    public interface IFormProxy<Form>
+    {
+        /// <summary>
+        /// Execute the given action in the thread that owns the form.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        public void Execute(Action<Form> action);
+
+        /// <summary>
+        /// Get the underlying form. This property should only be used to read values.
+        /// </summary>
+        public Form Read { get; }
+    }
+}
